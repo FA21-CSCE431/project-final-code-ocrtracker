5ebfa19bc8c386bf6b6b0ec1471123e2154abe6c
benrmn <bramon24@tamu.edu>
2021-09-21 15:38:05 -0500

    testing google OAUTH gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :books
   resources :ocrtrackers
-  root 'books#index'
+  root 'ocrtrackers#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
