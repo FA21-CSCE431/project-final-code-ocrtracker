46f4f142ee6a41e94f386c650eda3f6a50afd3a2
benrmn <bramon24@tamu.edu>
2021-09-21 16:06:09 -0500

    adding spec and OAUTH setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_12_193745) do
+ActiveRecord::Schema.define(version: 2021_09_21_204553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "books", force: :cascade do |t|
-    t.string "title"
+  create_table "admins", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "full_name"
+    t.string "uid"
+    t.string "avatar_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
   end
 
-  create_table "ocrtrackers", force: :cascade do |t|
-    t.string "WIP"
+  create_table "books", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
