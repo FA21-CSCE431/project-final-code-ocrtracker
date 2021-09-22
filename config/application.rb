faeb8fe5b29d3131ba4c1e10bd8c613e0d2432e5
benrmn <bramon24@tamu.edu>
2021-09-21 20:59:20 -0500

    testing OAUTH

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Ocrtracker
+module GoogleOauthDevise
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
