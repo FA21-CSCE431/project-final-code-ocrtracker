da22cf38eb51101dae0a6f05e92b2187665098a6
benrmn <bramon24@tamu.edu>
2021-09-24 12:20:24 -0500

    testing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   # resources :books
   # resources :ocrtrackers
-  root to: 'dashboards#show'
+  root to: 'ocrtrackers#show'
   devise_for :admins, controllers: { omniauth_callbacks: 'admins/omniauth_callbacks' }
   devise_scope :admin do
     get 'admins/sign_in', to: 'admins/sessions#new', as: :new_admin_session
