ec16edbce76ecb8a501137c6401e030df5e3e5ea
Hunter Baker <hbake@tamu.edu>
2021-09-28 16:30:37 +0000

    exercise model now validates more inputs, destroying an exercise will destroy its dependents

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,3 +1,6 @@
 class Exercise < ApplicationRecord
-    has_many :exercise_posts
+    has_many :exercise_posts, dependent: :destroy
+    validates :title, presence: true
+    validates :description, presence: true
+    validates :unit_name, presence: true
 end
