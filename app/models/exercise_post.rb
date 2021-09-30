375f2dd19564ecdf92544416b59133b0c3355f68
Hunter Baker <hbake@tamu.edu>
2021-09-30 23:22:06 +0000

    Add validation to models

--- app/models/exercise_post.rb
+++ app/models/exercise_post.rb
@@ -2,4 +2,5 @@ class ExercisePost < ApplicationRecord
     belongs_to :exercise
     belongs_to :workout_post
     has_many :exercise_submissions
+    validates :specific_instructions, presence: true
 end
--- app/models/workout_submission.rb
+++ app/models/workout_submission.rb
@@ -4,4 +4,6 @@ class WorkoutSubmission < ApplicationRecord
     has_many :exercise_submissions
     has_many :fistbumps
     has_one :leaderboard_spot
+
+    validates :submitted_datetime, presence: true
 end
