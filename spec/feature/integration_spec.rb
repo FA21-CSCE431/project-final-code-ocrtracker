12d78ce6ba639db9868a442af347232c3a9d0347
Hunter Baker <hbake@tamu.edu>
2021-10-02 19:26:17 -0500

    Add more text fixtures, small test for workout submission

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -1,7 +1,6 @@
 # location: spec/feature/integration_spec.rb
 require 'rails_helper'
 
-
 feature 'testing oauth' do
     fixtures :users
     scenario 'should create a new user' do
@@ -55,6 +54,23 @@ RSpec.describe 'Creating an exercise', type: :feature do
         login_as_user
         visit exercises_path
         expect(page).to have_content "You must be an admin to access this section"
+    end
+
+end
+
+RSpec.describe 'Submitting a workout', type: :feature do
+
+    # Include all of the current test fixtures
+    fixtures :users, :exercises, :workout_posts, :exercise_posts, :workout_submissions, :exercise_submissions
+
+    scenario 'user goes to the new submission page' do
+        login_as_user
+
+        # i.e. /submissions/new/5
+        visit "/submissions/new/#{workout_posts(:wp1).id}"
+
+        # Expect the page to have the title of the workout post
+        expect(page).to have_content("#{workout_posts(:wp1).title}")
     end
 
 end
