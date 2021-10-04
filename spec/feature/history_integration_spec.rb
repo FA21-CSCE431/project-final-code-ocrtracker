18f3c8286f11437d328d767fdff8f8f920d074c6
Hunter Baker <hbake@tamu.edu>
2021-10-04 18:02:26 -0500

    Add integration specs for history

---
+++ spec/feature/history_integration_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+# location: spec/feature/oauth_integration_spec.rb
+require 'rails_helper'
+
+RSpec.describe 'View submission history for a workout post', type: :feature do
+  fixtures :users, :exercises, :workout_posts, :exercise_posts, :workout_submissions, :exercise_submissions
+  
+  scenario 'admin views submission history for a workout post' do
+    login_as_admin
+
+    # Set a workout post
+    wp = workout_posts(:wp1)
+
+    visit "/submissions/history/#{wp.id}"
+    expect(page).to have_content(wp.ranked_exercise_post.exercise_submissions.first.unit_value)
+    
+  end
+
+  scenario 'user visits history path' do
+    login_as_user
+
+    wp = workout_posts(:wp1)
+    visit "/submissions/history/#{wp.id}"
+    expect(page).to have_content('You must be an admin to access this section')
+
+  end
+
+end
