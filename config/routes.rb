95ae25999bf6738e7670a717c3ac5c1aba8b43c7
Hunter Baker <hbake@tamu.edu>
2021-10-12 09:37:26 -0500

    Changed admin WOD route to /wod/set

--- config/routes.rb
+++ config/routes.rb
@@ -18,8 +18,8 @@ Rails.application.routes.draw do
   get 'posts/new/', to: 'posts#new'
   post 'posts/create/', to: 'posts#create'
 
-  get '/wod', to: 'wod#index'
-  post '/wod', to: 'wod#update_wod'
+  get '/wod/set', to: 'wod#index'
+  post '/wod/set', to: 'wod#update_wod'
 
   resources :exercises, :workout_posts, :exercise_posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
