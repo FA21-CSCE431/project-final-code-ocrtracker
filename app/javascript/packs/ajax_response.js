a397adc90ab6b569a3744259105f3c07b21ed5c1
Hunter Baker <hbake@tamu.edu>
2021-10-14 18:34:31 -0500

    Remove unused js file

--- app/javascript/packs/ajax_response.js
+++
@@ -1,5 +0,0 @@
-document.body.addEventListener("ajax:success", (event) => {
-    const [data, status, xhr] = event.detail;
-
-    document.getElementById('flash_message').innerHTML = data.message;
-});
