20ce316d3e7113780ca2b9204eb7406978953371
benrmn <bramon24@tamu.edu>
2021-10-23 16:22:58 -0500

    added permissions tests

--- spec/feature/permission_integration_spec.rb
+++ spec/feature/permission_integration_spec.rb
@@ -1,1 +1,28 @@
 # frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe 'Editing permissions', type: :feature do
+  fixtures :users
+
+  scenario 'valid check' do
+    login_as_admin
+    visit "/permissions"
+    page.check("[user][#{users(:admin_account).id}][is_admin]")
+    click_on 'Save Changes'
+    expect(page).to have_content 'Users successfully updated'
+  end
+
+  scenario 'valid uncheck' do
+    login_as_admin
+    visit "/permissions"
+    page.uncheck("[user][#{users(:admin_account).id}][is_admin]")
+    click_on 'Save Changes'
+    expect(page).to have_content 'Welcome to the User Dashboard!'
+  end
+
+  scenario 'non-admin user' do
+    login_as_user
+    visit "/permissions"
+    expect(page).to have_content 'You must be an admin to access this section'
+  end
+end
--- spec/feature/profile_integration_spec.rb
+++ spec/feature/profile_integration_spec.rb
@@ -21,4 +21,22 @@ RSpec.describe 'Editing an profile', type: :feature do
     click_on 'Save Changes'
     expect(page).to have_content 'new_test_desc'
   end
+  #
+  # scenario 'valid inputs url' do
+  #   login_as_admin
+  #   visit "/profiles/#{users(:admin_account).id}/edit"
+  #   fill_in 'Avatar url', with: 'https://fake_url.com'
+  #   # fill_in 'Picture', with: 'test_pic'
+  #   click_on 'Save Changes'
+  #   expect(page).to have_content 'https://fake_url.com'
+  # end
+  #
+  # scenario 'non-admin user url' do
+  #   login_as_user
+  #   visit "/profiles/#{users(:user_account).id}/edit"
+  #   fill_in 'Avatar url', with: 'https://fake_url.com'
+  #   # fill_in 'Picture', with: 'test_pic'
+  #   click_on 'Save Changes'
+  #   expect(page).to have_content 'https://fake_url.com'
+  # end
 end
