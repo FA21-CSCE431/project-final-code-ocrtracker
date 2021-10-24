0ab8bc9ccdebe89466dc1331b5b6d5f020244717
Hunter Baker <hbake@tamu.edu>
2021-10-24 16:38:48 -0500

    Group coding: Add leaderboard page, modify history page to function similarly, add opt_out field in submissions page

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,7 +2,16 @@
 
 # Dashboards Controller
 class DashboardsController < ApplicationController
+  before_action :set_ranked_eps, only: %i[leaderboard]
+
   def show; end
 
-  def leaderbaord; end
+  def leaderboard; end
+
+  private
+
+  def set_ranked_eps
+    @current_wod = WorkoutPost.current_wod
+    @ranked_eps = @current_wod.ranked_exercise_posts
+  end
 end
