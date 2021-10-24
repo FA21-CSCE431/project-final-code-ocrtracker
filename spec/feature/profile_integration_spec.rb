c499317420f75e6f8980fde159728bc694523aed
Hunter Baker <hbake@tamu.edu>
2021-10-24 17:41:14 -0500

    Group coding: Add fistbump calculations, fix profile specs for updated views

--- spec/feature/profile_integration_spec.rb
+++ spec/feature/profile_integration_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe 'Editing an profile', type: :feature do
   scenario 'valid inputs' do
     login_as_admin
     visit "/profiles/#{users(:admin_account).id}/edit"
-    fill_in 'Description', with: 'new_test_desc'
+    fill_in 'Edit Description', with: 'new_test_desc'
     # fill_in 'Picture', with: 'test_pic'
     click_on 'Save Changes'
     expect(page).to have_content 'new_test_desc'
@@ -17,7 +17,7 @@ RSpec.describe 'Editing an profile', type: :feature do
   scenario 'non-admin user' do
     login_as_user
     visit "/profiles/#{users(:user_account).id}/edit"
-    fill_in 'Description', with: 'new_test_desc'
+    fill_in 'Edit Description', with: 'new_test_desc'
     # fill_in 'Picture', with: 'test_pic'
     click_on 'Save Changes'
     expect(page).to have_content 'new_test_desc'
